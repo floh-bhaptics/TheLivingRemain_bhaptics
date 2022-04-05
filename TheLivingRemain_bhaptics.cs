257721b2ec5b3821c72a28af957b5a49db1eb047
floh-bhaptics <floh@fahrenbergers.de>
2022-04-05 21:01:31 +0200

    Projektdateien hinzufügen.

---
+++ TheLivingRemain_bhaptics.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MelonLoader;
+using HarmonyLib;
+using MyBhapticsTactsuit;
+
+
+namespace TheLivingRemain_bhaptics
+{
+    public class TheLivingRemain_bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+        
+        [HarmonyPatch(typeof(Handgun), "FireGun", new Type[] { })]
+        public class bhaptics_FireHandGun
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Handgun __instance)
+            {
+                bool isRight = (__instance.holdingHand == VRTK.GrabAttachMechanics.Hand.right);
+                tactsuitVr.Recoil("Pistol", isRight);
+            }
+        }
+        
+    }
+}
